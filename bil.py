e97cc3a7914c2986215aa79f040f9b8d557a252d
Morten Sjåfjell <150013555+mortensjafjell@users.noreply.github.com>
2024-11-07 15:58:13 +0100

    Add files via upload

--- bil.py
+++ bil.py
@@ -1,9 +1,9 @@
 """
 Tall brukt i programmet:
 Forsikring: Elbil: 5000 kr/år. Bensinbil: 7500 kr/år.
-Trafikkforsikringsavgift: 8,38 kr/dag for både elbil og bensinbil.    3058,7
-Drivstoffbruk: Elbil: 0,2 kWh/km. Strømpris (antar kun hjemmelading): 2.00 kr/kWh. Bensinbil: 1,0 kr/km.      4000
-Bomavgift: Elbil: 0,1 kr/km. Bensinbil: 0,3 kr/km.        1000
+Trafikkforsikringsavgift: 8,38 kr/dag for både elbil og bensinbil.
+Drivstoffbruk: Elbil: 0,2 kWh/km. Strømpris (antar kun hjemmelading): 2.00 kr/kWh. Bensinbil: 1,0 kr/km.
+Bomavgift: Elbil: 0,1 kr/km. Bensinbil: 0,3 kr/km.
 """
 
 km = 10000  # Antall årlige kilometer, felles for begge biltyper
